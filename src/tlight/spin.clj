22c588c59e73603aa925ed858ed812e59f263aeb
Tim Lightfoot <tim.lightfoot@gmail.com>
2020-06-08 22:15:14 +0800

    simplify and include ok function on (done).

--- src/tlight/spin.clj
+++ src/tlight/spin.clj
@@ -2,43 +2,39 @@
   (:require [clojure.core.async :as async :refer
              [<! >! >!! timeout chan alt! go go-loop]]))
 
-(def strings {:box1  "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
-              :box2  "⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓"
-              :box3  "⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆"
-              :box4  "⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋"
-              :box5  "⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁"
-              :box6  "⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈"
-              :box7  "⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈"
-              :spin1 "|/-\\"
-              :spin2 "◴◷◶◵"
-              :spin3 "◰◳◲◱"
-              :spin4 "◐◓◑◒"
-              :spin5 "▉▊▋▌▍▎▏▎▍▌▋▊▉"
-              :spin6 "▌▄▐▀"
-              :spin7 "╫╪"
-              :spin8 "■□▪▫"
-              :spin9 "←↑→↓"})
+(def spinners {:box1  "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+               :box2  "⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓"
+               :box3  "⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆"
+               :box4  "⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋"
+               :box5  "⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁"
+               :box6  "⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈"
+               :box7  "⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈"
+               :spin1 "|/-\\"
+               :spin2 "◴◷◶◵"
+               :spin3 "◰◳◲◱"
+               :spin4 "◐◓◑◒"
+               :spin5 "▉▊▋▌▍▎▏▎▍▌▋▊▉"
+               :spin6 "▌▄▐▀"
+               :spin7 "╫╪"
+               :spin8 "■□▪▫"
+               :spin9 "←↑→↓"})
 
 (def done? (atom (chan)))
+(defn done [] (>!! @done? true))
+(defn ok []
+  (print "✓")
+  (flush))
 
-(defn spin [& {:keys [type template ms]
-               :or   {type :box1 template "%s" ms 100}}]
+(defn spin [& {:keys [type ms]
+               :or   {type :box1, ms 100}}]
   "The spin function loops a spinner to STDOUT until a call to (done)"
-  (let [s      (get strings type)
-        length (count s)
-        ]
+  (let [frames (get spinners type)
+        length (count frames) ]
     (go-loop [i 0]
-      (let [c   (get s i)
-            i++ (mod (+ i 1) length)
-            out (format template c)
-            pre (format "\u001b[%sD" (count out))
-            ]
-        (print (str pre out))
+      (let [frame (get frames i)
+            i++   (mod (+ i 1) length)
+            clear "\033[1D"]
+        (print frame)
         (flush)
-        (alt!
-          @done? :done
-          (timeout ms) (recur i++))))))
-
-(defn done []
-  (>!! @done? true)) ;; done function
-
+        (print clear)
+        (alt! @done? (ok), (timeout ms) (recur i++))))))
